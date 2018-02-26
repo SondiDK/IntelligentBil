b6756d35ec51fa34ec9bf46c4580e02e3ac9f538
SondiDK <larseno@oregonstate.edu>
2018-02-26 13:49:53 +0100

    Initial commit

---
+++ app/src/main/java/dk/osl/intelligentbil/LoginActivity.java
@@ -0,0 +1,68 @@
+package dk.osl.intelligentbil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText usernameEt, passwordEt;
+    private Button loginBtn;
+    private static final String TAG = "LoginAkt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameEt = (EditText) findViewById(R.id.usrnametxt);
+        passwordEt = (EditText) findViewById(R.id.pwdtxt);
+
+        loginBtn = (Button) findViewById(R.id.loginbtn);
+
+        loginBtn.setOnClickListener(this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "onClick: login clicked");
+
+        if(view == loginBtn){
+           if(validateInput()) ;
+            login();
+        }
+
+    }
+
+    private boolean validateInput() {
+        Log.d(TAG, "validateInput: called");
+        boolean isValid = true;
+        
+        String username = usernameEt.getText().toString();
+        String password = usernameEt.getText().toString();
+        if(username.isEmpty() || password.isEmpty()){
+            Log.d(TAG, "validateInput: empty");
+            Toast.makeText(getBaseContext(),"Enter both fields", Toast.LENGTH_LONG).show();
+        }
+
+        return isValid;
+
+    }
+
+
+    public void login(){
+        String username = usernameEt.getText().toString();
+        String password = usernameEt.getText().toString();
+
+
+
+    }
+
+}
